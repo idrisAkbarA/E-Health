faf699439eac0d842f208eb44f04bef29d7cf711
Idris Akbar Adyusman <idrissmp32@gmail.com>
2021-06-17 08:37:46 +0700

    some error pages

---
+++ nuxt/components/error401.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-container fill-height>
+      <v-row
+        justify="center"
+        align="center"
+        align-content="center"
+        no-gutters
+      >
+        <v-col cols="12">
+          <v-img
+            max-width="35vw"
+            :src="'/noaccess.png'"
+            class="mt-n16 mx-auto"
+          ></v-img>
+
+        </v-col>
+        <v-col cols="12">
+          <h1 class="mt-n16 mx-auto text-center center-text">Hmm.. anda tidak punya hak akses kesini. </h1>
+        </v-col>
+      </v-row>
+      <v-row
+        justify="center"
+        no-gutters
+      >
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
---
+++ nuxt/components/error404.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-container fill-height>
+      <v-row
+        justify="center"
+        align="center"
+        align-content="center"
+        no-gutters
+      >
+        <v-col cols="12">
+          <v-img
+            max-width="35vw"
+            :src="'/notfound.png'"
+            class="mt-n16 mx-auto"
+          ></v-img>
+
+        </v-col>
+        <v-col cols="12">
+          <h4 class="mx-auto text-center center-text">Ikan hiu makan ikan</h4>
+          <h4 class="mx-auto text-center center-text">Halaman tidak ditemukan</h4>
+        </v-col>
+      </v-row>
+      <v-row
+        justify="center"
+        no-gutters
+      >
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
